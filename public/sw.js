bf1446b378ff9e3c69349de2fe18d01cc2990d94
Shreyan S <shreyanofficial25@gmail.com>
2025-08-03 16:57:55 +0000

    Added manifest.json and sw.js

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+
+// A basic service worker for PWA functionality
+self.addEventListener('fetch', (event) => {
+  // We are not adding any specific caching logic here for now.
+  // This file is primarily to make the web app installable.
+});
